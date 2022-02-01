accd483dc61854db1f8d5b0c72b8cbb61178d664
Romati-creator <82973693+Romati-creator@users.noreply.github.com>
2022-02-01 22:42:32 +0300

    homework3

---
+++ src/test/java/pages/components/StateAndCityComponents.java
@@ -0,0 +1,15 @@
+package pages.components;
+
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+
+public class StateAndCityComponents {
+    public void choiceStateAndCity() {
+        // выбираем State and Sity из выпадающего списка
+        $("#state").click();
+        $("#stateCity-wrapper").$(byText("Haryana")).click();
+        $("#city").click();
+        $("#stateCity-wrapper").$(byText("Karnal")).click();
+        $("#submit").click();
+    }
+}
